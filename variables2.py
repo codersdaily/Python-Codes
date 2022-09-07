adcd380f667ac35823b95ae9f23437e50f0168af
mradul010 <mradulmishra010@gmail.com>
2022-09-07 12:36:37 +0530

    09-07-2022

---
+++ variables2.py
@@ -0,0 +1,23 @@
+a = 25
+phy_Marks = 92
+che_Marks = 99
+b = 20
+print(a)
+print(b)
+total = phy_Marks + che_Marks
+print(total)
+percentage = (total/200) * 100
+print(percentage)
+'''
++
+-
+*
+/
+%
+=
+>
+<
+>=
+<=
+
+'''
